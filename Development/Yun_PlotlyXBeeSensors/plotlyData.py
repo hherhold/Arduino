9201d3b4420319c8be043a5c9b08b66b31080f4d
hherhold <hollister@fafoh.com>
2014-02-25 08:42:00 -0600

    Working plotly interface for 1 trace. (Woohoo!)

--- Development/Yun_PlotlyXBeeSensors/plotlyData.py
+++ Development/Yun_PlotlyXBeeSensors/plotlyData.py
@@ -4,30 +4,49 @@ import urllib2
 import time
 import sys
 from subprocess import call
+import plotly
+from platform import system
+
+if system() == 'Linux':
+    urlbase = 'http://localhost/arduino/'
+else:
+    urlbase = 'http://arduino.local/arduino/'
+
 
 ## On Linino, need to reset MCU here.
 
-sys.stdout.write( 'Getting coordinator...' )
-sys.stdout.flush()
-coordinatorAddress = urllib2.urlopen( 'http://arduino.local/arduino/coordinator' ).read()
-print( 'done.' )
-print( 'Coordinator: ' + coordinatorAddress )
-
-
-sys.stdout.write( 'Telling Coordinator to do discovery...' )
-sys.stdout.flush()
-dodisco = urllib2.urlopen( 'http://arduino.local/arduino/dodiscovery' )
-time.sleep( 5 )
-print( 'done.' )
-
+##sys.stdout.write( 'Getting coordinator...' )
+##sys.stdout.flush()
+##coordinatorAddress = urllib2.urlopen( urlbase + 'coordinator' ).read()
+##print( 'done.' )
+##print( 'Coordinator: ' + coordinatorAddress )
+##
+##
+##sys.stdout.write( 'Telling Coordinator to do discovery...' )
+##sys.stdout.flush()
+##dodisco = urllib2.urlopen( urlbase + 'dodiscovery' )
+##time.sleep( 5 )
+##print( 'done.' )
+##
 sys.stdout.write( 'Getting list of nodes...' )
 sys.stdout.flush();
-nodeString = urllib2.urlopen( 'http://arduino.local/arduino/listnodes' ).read()
+nodeString = urllib2.urlopen( urlbase + 'listnodes' ).read()
 print( 'done.' )
 
 nodeAddresses = nodeString.split()
 
-for node in nodeAddresses:
-    print node
+sys.stdout.write( 'Initializing plotly...' )
+sys.stdout.flush()
+py = plotly.plotly("hherhold", "831opc9jsj")
+print( 'done.' )
 
+index = 0
 
+while True:
+    measUrl = urlbase + 'measure/' + nodeAddresses[ 0 ]
+    print( 'Measure url = ' + measUrl );
+    temp = urllib2.urlopen( measUrl ).read()
+    print( temp )
+    py.plot( [ index ], [ float( temp ) ], filename='temp_1trace', fileopt='extend' )
+    time.sleep( 10 );
+    index = index + 1

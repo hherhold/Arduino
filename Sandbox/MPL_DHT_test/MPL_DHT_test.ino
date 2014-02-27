b097bc918fc09ea408bdb7648e719f2c72c230e6
Hollister Herhold <hollister@fafoh.com>
2014-02-27 16:17:41 -0600

    updated MPL DHT combo test with new library

--- Sandbox/MPL_DHT_test/MPL_DHT_test.ino
+++ Sandbox/MPL_DHT_test/MPL_DHT_test.ino
@@ -10,20 +10,53 @@
 #define DHT22_PIN 6
 
 dht DHT;
-MPL115A1 mpl( MPL115A1_SELECT_PIN, MPL115A1_ENABLE_PIN );
+MPL115A1 mpl;
 
 
 void setup()
 {
+    // initialize serial i/o
     Serial.begin(9600);
-
-    SPI.begin();
     
+    // initialize SPI interface
+    SPI.begin();
+
+    
+    // these are the defaults
+    //SPI.setDataMode(SPI_MODE0);
+    //SPI.setClockDivider(SPI_CLOCK_DIV4);  // MPL115A1 supports up to 8MHz
+    //SPI.setBitOrder(MSBFIRST);
+    
+    // initialize the chip select and enable pins
     pinMode(MPL115A1_SELECT_PIN, OUTPUT);
     pinMode(MPL115A1_ENABLE_PIN, OUTPUT);
     
+    // sleep the MPL115A1
+    digitalWrite(MPL115A1_ENABLE_PIN, LOW);
+
+    delay( 1000 );
+    
+    // wake the MPL115A1
+    digitalWrite(MPL115A1_ENABLE_PIN, HIGH);
+    delay(20);  // give the chip a few ms to wake up
+    
+    // set the chip select inactive, select signal is CS LOW
+    digitalWrite(MPL115A1_SELECT_PIN, HIGH);
+
+    mpl.init( MPL115A1_SELECT_PIN, MPL115A1_ENABLE_PIN );
+
+#if 0
+    Serial.begin(9600);
+
+    SPI.begin();
+
+    pinMode(MPL115A1_SELECT_PIN, OUTPUT);
+    pinMode(MPL115A1_ENABLE_PIN, OUTPUT);
+    
+    mpl.init( MPL115A1_SELECT_PIN, MPL115A1_ENABLE_PIN );    
     digitalWrite(MPL115A1_ENABLE_PIN, LOW);
     digitalWrite(MPL115A1_SELECT_PIN, HIGH);
+#endif
 }
 
 void loop()
@@ -31,13 +64,10 @@ void loop()
     digitalWrite(MPL115A1_ENABLE_PIN, HIGH);
     delay(20);  // give the chip a few ms to wake up
     
-    float pressure_pKa = mpl.calculatePressurekPa();
-    float tempC = mpl.calculateTemperatureC();
+    float pressure_pKa = mpl.calcPressure_kPa();
 
     Serial.print( "MPL115A1: Press (kPa) = " );
     Serial.print( pressure_pKa, 4 );
-    Serial.print( "  temp C =" );
-    Serial.print( tempC, 2 );
 
     // READ DATA
     Serial.print("\tDHT22 ");

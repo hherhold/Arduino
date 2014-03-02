f94ddf3c70b0fcfc62bb3ab9e4df7d011d2ed898
Hollister Herhold <hollister@fafoh.com>
2014-03-02 13:23:39 -0600

    Same as last comment. Bad commit.

--- libraries/MPL115A1/MPL115A1.cpp
+++ libraries/MPL115A1/MPL115A1.cpp
@@ -3,6 +3,8 @@
 
 #include "SPI.h"
 
+#define COEFFICIENT_DEBUG
+
 MPL115A1::MPL115A1( )
 {
 
@@ -56,12 +58,14 @@ void MPL115A1::readCoefficients( )
     // a0
     uint8_t A0_MSB = read( MPL115A1_A0_MSB_RD );
     uint8_t A0_LSB = read( MPL115A1_A0_LSB_RD );
-    uint32_t a0bits = ( A0_MSB << 8 ) | ( A0_LSB );
+    int32_t a0bits = ( A0_MSB << 8 ) | ( A0_LSB );
 
-    MPL115A1_coeff_a0 = ( ( a0bits & 0b0111111111111000 ) >> 3 ) +
-        ( ( float )( a0bits & 0b0111 ) / ( float )( 0b1000 ) );
-//    MPL115A1_coeff_a0 *=  ( a0bits & 0b1000000000000000 ) ? -1.0 : 1.0;
-//    Serial.print( "a0 = " ); Serial.println( MPL115A1_coeff_a0, 10 );
+    MPL115A1_coeff_a0 = ( float )( a0bits ) / ( float )( 0x1 << 3 );
+#ifdef COEFFICIENT_DEBUG
+    Serial.print( "a0 = 0x" ); Serial.print( a0bits, HEX );
+    Serial.print( ", " ); Serial.println( a0bits );
+    Serial.print( "a0 = " ); Serial.println( MPL115A1_coeff_a0, 10 );
+#endif
 
     // b1
     //      111 1 11
@@ -69,47 +73,32 @@ void MPL115A1::readCoefficients( )
     // b1 = SII.F FFFF FFFF FFFF
     uint8_t B1_MSB = read( MPL115A1_B1_MSB_RD );
     uint8_t B1_LSB = read( MPL115A1_B1_LSB_RD );
-#if 0
+    int32_t b1bits = ( B1_MSB << 8 ) | ( B1_LSB );
+    MPL115A1_coeff_b1 = ( float )( b1bits ) / ( float )( 0x1 << 13 );
+#ifdef COEFFICIENT_DEBUG
     Serial.print( "B1 msb = 0x" ); Serial.println( B1_MSB, HEX );
     Serial.print( "B1 Lsb = 0x" ); Serial.println( B1_LSB, HEX );
-    uint32_t b1bits = ( B1_MSB << 8 ) | ( B1_LSB );
     Serial.print( "B1 bits = 0x" ); Serial.println( b1bits, HEX );
-    MPL115A1_coeff_b1 = ( ( ( b1bits & 0b0110000000000000 ) >> 13 )  + 
-                          ( float )( b1bits & 0b0001111111111111 ) ) 
-        / ( float )( 0b10000000000000 );
-//    MPL115A1_coeff_b1 -= 3; // Two's complement notation
-//    Serial.print( "b1 = " ); Serial.println( MPL115A1_coeff_b1, 10 );
-    MPL115A1_coeff_b1 *= ( b1bits & 0b1000000000000000 ) ? -1.0 : 1.0;
+    Serial.print( "b1 = " ); Serial.println( MPL115A1_coeff_b1, 10 );
 #endif
-    // From sparkfun forums
-    MPL115A1_coeff_b1 = ( ( ( ( B1_MSB & 0x1F ) * 0x100 ) + B1_LSB ) / 8192.0 ) - 3;
-//    Serial.print( "b1 = " ); Serial.println( MPL115A1_coeff_b1, 10 );
 
     // b2
     uint8_t B2_MSB = read( MPL115A1_B2_MSB_RD );
     uint8_t B2_LSB = read( MPL115A1_B2_LSB_RD );
-#if 0
-    uint32_t b2bits = ( B2_MSB << 8 ) | ( B2_LSB );
-    MPL115A1_coeff_b2 = ( ( b2bits & 0b0100000000000000 ) >> 14 ) +
-        ( ( float )( b2bits & 0b001111111111111 ) / ( float )( 0b0100000000000000 ) );
-    MPL115A1_coeff_b2 *= ( b2bits & 0b1000000000000000 ) ? -1.0 : 1.0;
+    int32_t b2bits = ( B2_MSB << 8 ) | ( B2_LSB );
+    MPL115A1_coeff_b2 = ( float )( b2bits ) / ( float )( 0x1 << 14 );
+#ifdef COEFFICIENT_DEBUG    
+    Serial.print( "b2 = " ); Serial.println( MPL115A1_coeff_b2, 10 );
 #endif
-    // From sparkfun forums
-    MPL115A1_coeff_b2 = ( ( ( ( B2_MSB - 0x80 ) << 8 ) + B2_LSB ) / 16384.0 ) - 2;
-//    Serial.print( "b2 = " ); Serial.println( MPL115A1_coeff_b2, 10 );
 
     // c12 is the oddball here in that it's 14 bits, with the 2 LSB set to zero.
     uint8_t C12_MSB = read( MPL115A1_C12_MSB_RD );
     uint8_t C12_LSB = read( MPL115A1_C12_LSB_RD );
-#if 0
-    uint32_t c12bits = ( ( C12_MSB << 8 ) | ( C12_LSB ) ) >> 2;
-    MPL115A1_coeff_c12 = 
-        ( ( c12bits & 0b01111111111111 ) / ( float )( 0b10000000000000000000000 ) );
-    MPL115A1_coeff_c12 *= ( c12bits & 0b10000000000000 ) ? -1.0 : 1.0;
+    int32_t c12bits = ( C12_MSB << 8 ) | ( C12_LSB );
+    MPL115A1_coeff_c12 = ( float )( c12bits ) / 0x1000000;
+#ifdef COEFFICIENT_DEBUG
+    Serial.print( "c12 = " ); Serial.println( MPL115A1_coeff_c12, 10 );
 #endif
-    // From sparkfun forums
-    MPL115A1_coeff_c12 = ( ( ( C12_MSB * 0x100 ) + C12_LSB ) / 16777216.0 );
-//    Serial.print( "c12 = " ); Serial.println( MPL115A1_coeff_c12, 10 );
 }
 
 uint32_t MPL115A1::getTemp_counts( )
